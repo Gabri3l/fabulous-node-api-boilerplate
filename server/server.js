b9cfdb411dac8196fdfa73bb4b5cfc12ad4b54eb
Gabriele Cimato <gabriele.cimato@gmail.com>
2016-08-17 17:31:53 -0700

    feat: add api entry point with server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import api from './api/api';
+import config from './config/config';
+import mongoose from 'mongoose';
+import logger from './util/logger';
+
+const app = express();
+
+// db.url is different depending on NODE_ENV
+mongoose.connect(config.db.url);
+mongoose.Promise = global.Promise;
+
+// setup the app middlware
+require('./middleware/appMiddleware')(app);
+
+// setup the api
+app.use('/api/v2/', api);
+
+// export the app for testing
+module.exports = app;
